438220c554b52e4a8ed05ccff008e6044ccbc9c5
Nahueldzn19 <97405387+Nahueldzn19@users.noreply.github.com>
2022-01-09 20:36:41 -0300

    Add files via upload

---
+++ src/version.js
@@ -0,0 +1,32 @@
+const version = (prefix, pushname) => {
+    return `
+*Nahuel bot by nahuel.dzn*
+
+*Actualizado:* 07 de Enero del 2022
+*Versión actual:* 4.5
+*Ofrecida por:* nahuel.dzn™
+
+
+*INFORME*
+
+Si no funciona el comando *play o *play2 checa el blog del bot y mira la version que poses 
+Ya que ire cambiando mi apikey de mi bot para que siga en funcionamiento
+Cambiare la apikey pasando un mes o si veo muchos atercados o robos
+Si vez a alguien que robo mi bot comunicate conmigo con el comando *creador
+
+
+*SIGUEME EN YOUTUBE*
+
+https://youtube.com/channel/UCMSnq-4NumQeC6Kk-Dnaoxw
+
+*SIGUEME EN INSTAGRAM*
+
+https://www.instagram.com/nahuel.dzn
+
+
+_*by: nahuel.dzn*_
+`
+
+}
+
+exports.version = version
